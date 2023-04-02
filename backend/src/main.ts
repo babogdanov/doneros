9861a8ae8ef9a4c7d41497970b24d7412322aa09
babogdanov <boyan.bogdanov@accedia.com>
2023-04-02 14:24:46 +0300

    add global configuration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,12 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { configure } from './main.config'
+
+import 'reflect-metadata'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+  configure(app)
+  await app.listen(process.env.PORT!)
 }
-bootstrap();
+bootstrap()
