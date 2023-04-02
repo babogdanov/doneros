6122d98ca593e016139b3cd5ac17bc0d602c5c3a
babogdanov <boyan.bogdanov@accedia.com>
2023-04-02 14:25:53 +0300

    validate root module config, configure typeorm

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,47 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config'
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import * as Joi from 'joi'
+
+import { UserModule } from '@api'
+import { ServicesModule } from '@services/services.module'
+import { getConnectionOptions } from 'typeorm'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    // Configuration Modules
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test', 'provision')
+          .required(),
+        PORT: Joi.number().required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+        JWT_SECRET: Joi.string().required(),
+        JWT_TIMEOUT: Joi.string().required(),
+        PASSWORD_RESET_TOKEN_EXPIRATION: Joi.number().required(),
+      }),
+      validationOptions: {
+        allowUnknown: true,
+        abortEarly: true,
+      },
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: async () =>
+        Object.assign(await getConnectionOptions(), {
+          entities: ['dist/**/*.entity{.ts,.js}'],
+          migrations: ['dist/services/typeorm/migrations/*.{.ts,.js}'],
+        }),
+    }),
+    // App Modules
+    UserModule,
+    ServicesModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
