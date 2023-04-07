bd8ede7655263ebc03e0748ed62b33f050878681
babogdanov <boyan.bogdanov@accedia.com>
2023-04-08 00:14:34 +0300

    Normalize linting and formatting across project

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -28,27 +28,16 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
-    'plugin:jsx-a11y/recommended',
-    'plugin:prettier/recommended', // Make sure this is always the last element in the array.
   ],
-  plugins: ['simple-import-sort', 'prettier'],
+  plugins: ['react'],
   rules: {
     semi: 0,
+    'no-console': 1,
+    quotes: [2, 'single', { avoidEscape: true }],
+    'jsx-quotes': [2, 'prefer-single'],
     '@typescript-eslint/ban-ts-comment': 'off',
-    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
     'react/react-in-jsx-scope': 'off',
-    'jsx-a11y/accessible-emoji': 'off',
     'react/prop-types': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
-    'simple-import-sort/imports': 'error',
-    'simple-import-sort/exports': 'error',
-    'jsx-a11y/anchor-is-valid': [
-      'error',
-      {
-        components: ['Link'],
-        specialLink: ['hrefLeft', 'hrefRight'],
-        aspects: ['invalidHref', 'preferButton'],
-      },
-    ],
   },
 }
