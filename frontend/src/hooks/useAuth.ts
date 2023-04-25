8ba21ec324c5d6bf4613c5b4bef20b34ac545ea1
Boyan Bogdanov <boyan.bogdan@gmail.com>
2023-04-25 12:50:00 +0300

    chore: prettier autoformatting

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -6,9 +6,12 @@ export const LOCAL_AUTH_KEY = ['/auth/sync']
 const useAuth = (): AuthState => {
   const baseState = { user: null, accessToken: null, isAuthenticated: false }
   const queryClient = useQueryClient()
-  const data = queryClient.getQueryData(LOCAL_AUTH_KEY) as Omit<AuthState,'isAuthenticated'>
+  const data = queryClient.getQueryData(LOCAL_AUTH_KEY) as Omit<
+    AuthState,
+    'isAuthenticated'
+  >
   const isAuthenticated = !!data
-  const res = {...data, isAuthenticated}
+  const res = { ...data, isAuthenticated }
   return data ? res : baseState
 }
 
