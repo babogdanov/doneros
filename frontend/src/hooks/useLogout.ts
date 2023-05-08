14b5b4ed252e2e168ee2c153c867a91e703b05ba
babogdanov <boyan.bogdanov@accedia.com>
2023-05-08 17:14:35 +0300

    feat(FE): add logout function and button

---
+++ frontend/src/hooks/useLogout.ts
@@ -0,0 +1,21 @@
+import { useCallback } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useQueryClient } from '@tanstack/react-query'
+
+import { AUTH_QUERY_KEY } from '../api/hooks/queries/auth/useSyncQuery'
+import { clearUser } from '../utils/local-storage.utils'
+
+const useLogout = () => {
+  const queryClient = useQueryClient()
+  const navigate = useNavigate()
+
+  const onLogout = useCallback(() => {
+    queryClient.setQueryData(AUTH_QUERY_KEY, null)
+    clearUser()
+    navigate('/login')
+  }, [navigate, queryClient])
+
+  return onLogout
+}
+
+export default useLogout
