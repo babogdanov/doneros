bafae86c1cba936d1307b280d1b298664dc4f433
Boyan Bogdanov <boyan.bogdan@gmail.com>
2023-06-11 19:59:16 +0300

    feat(FE): add phone number input in register page, update styles

--- frontend/src/pages/Register.page.tsx
+++ frontend/src/pages/Register.page.tsx
@@ -6,11 +6,12 @@ import useValidation from '../hooks/useValidation'
 import { trimObjectStrings } from '../utils/string.utils'
 import { ERROR } from '../utils/error-message.constants'
 
-const RegisterPage = () => {
+const Register = () => {
   const [formState, setFormState] = useState({
     email: '',
     password: '',
     confirmPassword: '',
+    phoneNumber: '',
   })
 
   const { mutate: register } = useRegisterMutation()
@@ -31,6 +32,13 @@ const RegisterPage = () => {
       validationFunc: ([passwordOne, passwordTwo]) => passwordOne === passwordTwo,
       errorMessage: ERROR.register.confirmPassword,
     },
+    {
+      input: formState.phoneNumber,
+      validationFunc: (input) => {
+        return typeof input === 'string' ? input.match(/\d/g)?.length === 10 : false
+      },
+      errorMessage: ERROR.register.phoneNumberRequired,
+    },
   ])
 
   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
@@ -43,18 +51,20 @@ const RegisterPage = () => {
 
     const trimmedForm = trimObjectStrings(formState)
 
-    register({ email: trimmedForm.email, password: trimmedForm.password })
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { confirmPassword, ...rest } = trimmedForm
+    register(rest)
   }
 
   return (
     <>
       <form
-        className='w-full h-full flex flex-col items-center justify-center'
+        className='flex h-full w-full flex-col items-center justify-center'
         onSubmit={handleSubmit}
       >
-        <div className='w-full max-w-xl bg-transparentrounded-2xl p-8'>
+        <div className='w-full max-w-xl rounded-2xl bg-transparent p-8'>
           <div className='text-center'>
-            <h1 className='text-yellow-500 font-bold text-5xl'>Register</h1>
+            <h1 className='text-5xl font-bold text-yellow-500'>Register</h1>
           </div>
         </div>
         <div className='mt-8'>
@@ -62,7 +72,7 @@ const RegisterPage = () => {
             type='email'
             name='email'
             placeholder='Email'
-            className='block w-full bg-gray-800 text-gray-900 rounded-lg focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:border-transparent'
+            className='w-full rounded-lg focus:ring-2 focus:ring-yellow-500'
             value={formState.email}
             onChange={(event) =>
               setFormState((prevState) => ({
@@ -73,10 +83,23 @@ const RegisterPage = () => {
           />
 
           <input
+            type='text'
+            name='phone'
+            placeholder='Phone number'
+            className='w-full rounded-lg focus:ring-2 focus:ring-yellow-500'
+            value={formState.phoneNumber}
+            onChange={(event) =>
+              setFormState((prevState) => ({
+                ...prevState,
+                phoneNumber: event.target.value,
+              }))
+            }
+          />
+          <input
             type='password'
             name='password'
             placeholder='Password'
-            className='w-full py-3 px-4 bg-yellow-500 text-gray-900 rounded-lg font-bold hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-opacity-50'
+            className='w-full rounded-lg bg-yellow-500 px-4 py-3 font-bold text-gray-900 hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-yellow-500'
             value={formState.password}
             onChange={(event) =>
               setFormState((prevState) => ({
@@ -89,7 +112,7 @@ const RegisterPage = () => {
             type='password'
             name='password'
             placeholder='Password'
-            className='w-full py-3 px-4 bg-yellow-500 text-gray-900 rounded-lg font-bold hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-opacity-50'
+            className='w-full rounded-lg bg-yellow-500 px-4 py-3 font-bold text-gray-900 hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-yellow-500'
             value={formState.confirmPassword}
             onChange={(event) =>
               setFormState((prevState) => ({
@@ -100,7 +123,7 @@ const RegisterPage = () => {
           />
           <button
             type='submit'
-            className='w-full flex justify-center items-center text-2x1 py-3 px-4 text-slate-300 hover:text-slate-50 text-2xl hover:text-3xl bg-yellow-500 rounded-lg focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-opacity-50'
+            className='flex w-full items-center justify-center rounded-lg bg-yellow-500 px-4 py-3 text-2xl text-slate-300 hover:text-3xl hover:text-slate-50 focus:outline-none focus:ring-2 focus:ring-yellow-500'
           >
             Register
           </button>
@@ -110,4 +133,4 @@ const RegisterPage = () => {
   )
 }
 
-export default RegisterPage
+export default Register
