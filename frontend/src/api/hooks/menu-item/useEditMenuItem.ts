bdf8a8fe38f4baa0956106749ac07cc0a82ac1c5
Boyan Bogdanov <boyan.bogdan@gmail.com>
2023-06-11 14:03:57 +0300

    feat(FE): add redirects to menu item list page after successful create or update

--- frontend/src/api/hooks/menu-item/useEditMenuItem.ts
+++ frontend/src/api/hooks/menu-item/useEditMenuItem.ts
@@ -1,9 +1,14 @@
+import { useNavigate } from 'react-router-dom'
 import { MenuItemRequest, MenuItemResponse } from '../../../types/menu-item'
 import useBaseMutation from '../useBaseMutation'
 
 const useEditMenuItem = (id: string) => {
+  const navigate = useNavigate()
   return useBaseMutation<MenuItemRequest, MenuItemResponse>({
     apiParams: { path: `/menu-item/${id}`, method: 'PUT' },
+    builtInParams: {
+      onSuccess: (data) => navigate(`/restaurants/${data.menuItem.restaurant.id}`),
+    },
   })
 }
 
