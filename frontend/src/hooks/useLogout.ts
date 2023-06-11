34378097a472cbb0eba4496e77172d92a952f122
Boyan Bogdanov <boyan.bogdan@gmail.com>
2023-06-11 20:05:40 +0300

    chore(FE): clear cart store on logout

--- frontend/src/hooks/useLogout.ts
+++ frontend/src/hooks/useLogout.ts
@@ -4,14 +4,17 @@ import { useQueryClient } from '@tanstack/react-query'
 
 import { AUTH_QUERY_KEY } from '../api/hooks/auth/queries/useSyncQuery'
 import { clearUser } from '../utils/local-storage.utils'
+import useCartStore from './zustand/useCartStore'
 
 const useLogout = () => {
   const queryClient = useQueryClient()
   const navigate = useNavigate()
+  const resetCart = useCartStore((state) => state.reset)
 
   const onLogout = useCallback(() => {
     queryClient.setQueryData(AUTH_QUERY_KEY, null)
     clearUser()
+    resetCart()
     navigate('/login')
   }, [navigate, queryClient])
 
