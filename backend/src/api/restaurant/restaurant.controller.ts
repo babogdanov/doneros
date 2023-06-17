a0f206bb4264f3bca24b58c2a01603386ab1415f
Boyan Bogdanov <boyan.bogdan@gmail.com>
2023-06-17 22:11:46 +0300

    feat(FE): decorate endpoints that require special roles with the appropriate guards

--- backend/src/api/restaurant/restaurant.controller.ts
+++ backend/src/api/restaurant/restaurant.controller.ts
@@ -1,4 +1,16 @@
-import { Controller, Get, Post, Body, Param, Delete, Put } from '@nestjs/common'
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Put,
+  UseGuards,
+} from '@nestjs/common'
+import { UserRole } from '@entities/user.entity'
+import { RolesGuard } from 'src/auth/guards/role.guard'
+import { JwtAuthGuard } from '@guards/jwt.guard'
 import { RestaurantService } from './restaurant.service'
 import { CreateRestaurantDto } from './dto/create-restaurant.dto'
 import { UpdateRestaurantDto } from './dto/update-restaurant.dto'
@@ -19,6 +31,7 @@ export class RestaurantController {
     return { restaurant }
   }
 
+  @UseGuards(JwtAuthGuard, new RolesGuard([UserRole.ADMIN, UserRole.MANAGER]))
   @Post()
   async create(@Body() createRestaurantDto: CreateRestaurantDto) {
     const restaurant = await this.restaurantService.createRestaurant(
@@ -27,6 +40,7 @@ export class RestaurantController {
     return { restaurant }
   }
 
+  @UseGuards(JwtAuthGuard, new RolesGuard([UserRole.ADMIN, UserRole.MANAGER]))
   @Put(':id')
   async update(
     @Param('id') id: string,
@@ -39,6 +53,7 @@ export class RestaurantController {
     return { restaurant }
   }
 
+  @UseGuards(JwtAuthGuard, new RolesGuard([UserRole.ADMIN, UserRole.MANAGER]))
   @Delete(':id')
   async remove(@Param('id') id: string) {
     const restaurant = await this.restaurantService.removeRestaurant(+id)
