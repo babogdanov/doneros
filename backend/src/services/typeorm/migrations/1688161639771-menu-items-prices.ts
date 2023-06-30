d8f4f390a65f14e2ef12edd4ab803aa85608b899
Daniel Dimitrov <56864621+DidoWP@users.noreply.github.com>
2023-07-01 00:52:16 +0300

    chore: commit

--- backend/src/services/typeorm/migrations/1688161639771-menu-items-prices.ts
+++ backend/src/services/typeorm/migrations/1688161639771-menu-items-prices.ts
@@ -1,14 +1,15 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm'
 
 export class menuItemsPrices1688161639771 implements MigrationInterface {
-    name = 'menuItemsPrices1688161639771'
+  name = 'menuItemsPrices1688161639771'
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "menu_item" ADD "price" numeric(10,2) NOT NULL DEFAULT '0'`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "menu_item" ADD "price" numeric(10,2) NOT NULL DEFAULT '0'`,
+    )
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "menu_item" DROP COLUMN "price"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "menu_item" DROP COLUMN "price"`)
+  }
 }
