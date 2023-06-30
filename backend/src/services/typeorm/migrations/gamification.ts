e9aa3405cfea67eb491e6fc34688181e166b3602
Daniel Dimitrov <56864621+DidoWP@users.noreply.github.com>
2023-07-01 00:35:26 +0300

    chore: styling

--- backend/src/services/typeorm/migrations/gamification.ts
+++ backend/src/services/typeorm/migrations/gamification.ts
@@ -1,19 +1,19 @@
 import { MigrationInterface, QueryRunner } from 'typeorm'
 
 export class user1680432115643 implements MigrationInterface {
-  name = 'user1680432115643'
+  name = ''
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       `CREATE TABLE "users" (
         "id" SERIAL NOT NULL,
-        "created_at" TIMESTAMP NOT NULL DEFAULT NOW(), 
+        "userId" 
         "updated_at" TIMESTAMP NOT NULL DEFAULT NOW(), 
-        "email" character varying NOT NULL, 
-        "password" character varying NOT NULL,        
-
+        "points" integer,
+        
         CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"), 
-        CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`,
+        CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id")
+      )`,
     )
   }
 
---
+++ backend/src/services/typeorm/seeds/1687722002937-gamification.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class gamification1687722002937 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
