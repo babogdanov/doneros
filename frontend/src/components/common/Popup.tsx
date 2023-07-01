66bb1db9f35a7ed5ebd45cbeef6822b71d4c5b4b
Daniel Dimitrov <56864621+DidoWP@users.noreply.github.com>
2023-07-01 17:21:02 +0300

    chore: popup props (not the best)

--- frontend/src/components/common/Popup.tsx
+++ frontend/src/components/common/Popup.tsx
@@ -1,17 +1,17 @@
-const Popup = (props) => {
-  return (
-    <div className='fixed inset-0 bg-black/50'>
-      <div className='relative mx-auto mt-[calc(100vh-85vh-20px)] h-auto max-h-[70vh] w-5/6 overflow-auto rounded-md border border-gray-400 bg-white p-20'>
-        <span
-          className='fixed right-[calc(9%)] top-[calc(100vh-85vh-20px)] flex h-20 w-20 cursor-pointer items-center justify-center rounded-full border border-gray-400 bg-gray-300 text-lg'
-          onClick={props.handleClose}
-        >
-          x
-        </span>
-        {props.content}
-      </div>
+import { MouseEventHandler, ReactChild, ReactFragment, ReactPortal } from 'react'
+
+const Popup = (props: { handleClose: MouseEventHandler<HTMLSpanElement> | undefined; content: boolean | ReactChild | ReactFragment | ReactPortal | null | undefined }) => (
+  <div className='fixed inset-0 bg-black/50'>
+    <div className='relative mx-auto mt-[calc(100vh-85vh-20px)] h-auto max-h-[70vh] w-5/6 overflow-auto rounded-md border border-gray-400 bg-white p-20'>
+      <span
+        className='fixed right-[calc(9%)] top-[calc(100vh-85vh-20px)] flex h-20 w-20 cursor-pointer items-center justify-center rounded-full border border-gray-400 bg-gray-300 text-lg'
+        onClick={props.handleClose}
+      >
+        x
+      </span>
+      {props.content}
     </div>
-  )
-}
+  </div>
+)
 
 export default Popup
