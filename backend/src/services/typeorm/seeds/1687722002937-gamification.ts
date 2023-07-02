17036e8a8d1b6870f5c37d8b4f3efdb205134828
Daniel Dimitrov <56864621+DidoWP@users.noreply.github.com>
2023-07-02 20:02:50 +0300

    chore: commit

--- backend/src/services/typeorm/seeds/1687722002937-gamification.ts
+++ backend/src/services/typeorm/seeds/1687722002937-gamification.ts
@@ -1,11 +1,7 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm'
 
 export class gamification1687722002937 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {}
 }
